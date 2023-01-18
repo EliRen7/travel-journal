a14acf52a4b213e4be77002306c45fef7a4a5487
Elizabeth Renderos <84453538+EliRen7@users.noreply.github.com>
2023-01-18 15:49:30 +0900

    Update NavBar.js

--- NavBar.js
+++ NavBar.js
@@ -4,12 +4,12 @@ import React from 'react'
 
 export default function NavBar() {
     return(
-    <>
+    <div>
         <nav className="nav">
         <img src= "../images/earthlogo.png" className="navbar-img"/> 
         <span className="navbar-title">my travel journal</span>
         </nav>
         
-    </>
+    </div>
     )
 }
